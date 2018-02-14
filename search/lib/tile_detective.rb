d06f7b76b3ae3303609e95ef856d21e00d43d39b
Edward Dal Santo <edalsanto@doximity.com>
2018-02-13 21:08:04 -0800

    rotation and base case

--- search/lib/tile_detective.rb
+++ search/lib/tile_detective.rb
@@ -1,19 +1,50 @@
 class TileDetective
-  TILE = [
-    [ true, true ],
-    [ false, true ]
-  ]
+
+  def self.new_tile
+    [
+      [ true, true ],
+      [ false, true ]
+    ]
+  end
 
   # Given a square and a missing point in the square
   def self.solve(square, missing_point)
-    # Base case:
-    #   square.size is equal to 2
-    #   rotate tile until fit
-    #     fit => all trues in square
-    #       if already true, leave it
-    #   place tile
-    #
-    # otherwise, divide square into 4 equal subsquares of the same problem
+    ## Not super optimized
+
+    n = square.size
+
+    # Base case: square.size is equal to 2
+    if n == 2
+      tile = new_tile
+
+      # rotate tile until fit
+      while (tile[missing_point[0]][missing_point[1]] == true) do
+        rotate(tile)
+      end
+
+      # place tile in right place
+      for i in (0...2)
+        for j in (0...2)
+          # Skip filled in 1
+          next if [i, j] == missing_point
+
+          square[i][j] = tile[i][j]
+        end
+      end
+    else # divide square into 4 equal subsquares of the same problem
+      # place tile in center
+      y = ((n / 2) - 1)
+      center_square = [
+        [ y, y ],
+        [ y, y + 1 ],
+        [ y + 1, y ],
+        [ y + 1, y + 1]
+      ]
+      filled_quadrant = if missing_point[0] >= n /  2
+        #...
+                        end
+    end
+
     #   place tile in center such that each subsquare now has a missing point => you now have 4 identical subproblems
     #   recursively call solve on:
     #     square_size / 2, p1
@@ -23,7 +54,16 @@ class TileDetective
     #
   end
 
-  def self.rotate_tile
-    # Figure out how to rotate matrix by 90 degrees
+  # Rotate matrix in place
+  def self.rotate(tile)
+    # Exchange top rows
+    tile.reverse!
+
+    # Swap corresponding rows
+    for i in (0...tile.length)
+      for j in ((i + 1)...tile[i].length)
+        tile[i][j], tile[j][i] = tile[j][i], tile[i][j]
+      end
+    end
   end
 end
--- search/spec/tile_detective_spec.rb
+++ search/spec/tile_detective_spec.rb
@@ -1,33 +1,58 @@
 require "tile_detective"
 
 describe TileDetective do
-  it "returns a finished board" do
-    square = [
-               [ false, false ],
-               [ false, true ]
-            ]
-    expect described_class.solve(square, [1, 1]).to eq(
-      [
-        [true, true],
-        [true, true]
-      ]
-    )
+  describe ".solve" do
+    it "returns a finished 2 x 2 board" do
+      square = [
+                 [ false, false ],
+                 [ false, true ]
+              ]
+
+      described_class.solve(square, [1, 1])
+
+      expect(square).to eq(
+        [
+          [true, true],
+          [true, true]
+        ]
+      )
+    end
+
+    it "returns a finished board" do
+      square = [
+                 [ false, false ],
+                 [ false, true ],
+                 [ false, false ],
+                 [ false, false ]
+              ]
+      expect described_class.solve(square, [1, 1]).to eq(
+        [
+          [ true, true ],
+          [ true, true ],
+          [ true, true ],
+          [ true, true ]
+        ]
+      )
+    end
   end
 
-  it "returns a finished board" do
-    square = [
-               [ false, false ],
-               [ false, true ]
-               [ false, false ],
-               [ false, false ]
-            ]
-    expect described_class.solve(square, [1, 1]).to eq(
-      [
-        [ true, true ],
-        [ true, true ]
-        [ true, true ],
-        [ true, true ]
+  describe ".rotate" do
+    it "rotates a matrix" do
+      matrix = [
+        [ 1, 2, 3 ],
+        [ 4, 5, 6 ],
+        [ 7, 8, 9 ]
       ]
-    )
+
+      described_class.rotate(matrix)
+
+      expect(matrix).to eq(
+        [
+          [ 7, 4, 1 ],
+          [ 8, 5, 2 ],
+          [ 9, 6, 3 ]
+        ]
+      )
+    end
   end
 end
